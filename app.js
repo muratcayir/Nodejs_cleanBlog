d6cf94c2c4669fdb7ce895982d302172ae2e7e6a
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-05-26 19:44:57 +0300

    MongoDB ve CRUD eklendi

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Post = require('./models/Post');
 
 const app = express();
+
+//
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set('view engine', 'ejs');
@@ -11,35 +19,29 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //ROUTES
-app.get('/index', (req, res) => {
 
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+     posts });
 });
 
 app.get('/about', (req, res) => {
-
   res.render('about');
 });
 
-
-
-
 app.get('/post', (req, res) => {
-
   res.render('post');
 });
 
-
-
 app.get('/add_post', (req, res) => {
-
   res.render('add_post');
 });
 
-
-
-
-
+app.post('/posts', async (req, res) => { // async - await yapısı kullanacğız.
+  await Post.create(req.body)            // body bilgisini Post modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+  res.redirect('/')
+});
 
 const port = 5000;
 app.listen(port, () => {
